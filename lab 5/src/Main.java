6a538841df0ac7347b99b871bdcdf912a1d327c6
fuckkilla <f.cloudtamer@gmail.com>
2022-12-25 20:45:27 +0200

    other labs

--- lab 5/src/Main.java
+++ lab 5/src/Main.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Person basicPerson = new Person("Ivan","Bannyk","19");
-        Person studentPerson = new Student("Ivan","Bannyk","19","K19.1","17092");
-        Person lecturePerson = new Lecturer("Ivan","Bannyk","19", "programming","10000");
+        Person basicPerson = new Person("Bohdan","Fuchilo","18");
+        Person studentPerson = new Student("Bohdan","Fuchilo","18","K19.1","0512");
+        Person lecturePerson = new Lecturer("Bohdan","Fuchilo","18", "OOP","5000");
         ArrayList<Person> myArrayForTest = new ArrayList<Person>();
 
         myArrayForTest.add(basicPerson);
